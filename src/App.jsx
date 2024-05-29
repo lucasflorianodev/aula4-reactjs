b45c111c918932178b9a13066295116e6ff93fa5
lucasflorianodev <lucasfloriano09@gmail.com>
2024-05-29 08:33:42 -0300

    deploy aula4

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MyComponent from './components/MyComponent';
+import StyledComponent from './components/StyledComponent';
+import Campanha from './components/Campanha';
+import FormularioDeContato from './components/FormularioDeContato';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Formulário de Contato</h1>
+      <FormularioDeContato />
+    </div>
+  );
+}
+
+export default App;
